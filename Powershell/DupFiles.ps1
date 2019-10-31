7f5866409c0ddcc5fc13499b12a8d34f4ceb3af2
Cyb3r4rch3r <6243762+Cyb3r4rch3r@users.noreply.github.com>
2019-10-31 09:33:33 -0400

    Update DupFiles.ps1

--- Powershell/DupFiles.ps1
+++ Powershell/DupFiles.ps1
@@ -1,11 +1,13 @@
-# I didn't write this, but I can't remember where I got it from. 
+# I didn't write this orginally, but I've modified it slightly. Also, I can't remember where I got it from. 
 # If someone finds it, please let me know so I can credit the author appropriately
 function get-md5hash {[System.BitConverter]::ToString((new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider).ComputeHash([System.IO.File]::ReadAllBytes($args)))}
 
 
-$searchpath = "<ENTER PATH HERE>"
+$searchpath = Read-Host -Prompt "Enter the path/directories to search"
 
-$savepath = "<ENTER PATH HERE>\DupFiles.csv"
+$savepath = Read-Host -Prompt "Enter the directory to save the report to"
+
+$saveFile = "\DupFiles.csv"
 
 Get-ChildItem $searchpath -Recurse|`
 
@@ -15,4 +17,4 @@ Select-Object Name,Fullname,CreationTime,LastWriteTime,Length,@{Name="MD5";Expre
 
 group MD5|?{$_.Count -gt 1}|%{$_.Group}|sort MD5|`
 
-Export-Csv $savepath -NoTypeInformation -Encoding "Unicode"
+Export-Csv $savepath$saveFile -NoTypeInformation -Encoding "Unicode"
